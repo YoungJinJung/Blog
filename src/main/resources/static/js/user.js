9a363af4bd5a3770d3ff8949fd0caec982e67324
kuma.jung <yj.jung5078@gmail.com>
2022-06-05 21:05:07 +0900

    add email verify and password valid

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -80,4 +80,85 @@ let index = {
     }*/
 }
 
+$("#username").blur(function(){
+    let inputUserName = $("#username").val();
+    if(inputUserName == "" || inputUserName.length < 2){
+        $(".checkNameComment").text("이름은 2자 이상 8자 이하로 설정해주세요 :)");
+        $(".checkNameComment").css("color", "red");
+        $("#doubleCheckName").val("false");
+    }else{
+        $.ajax({
+            url : '/auth/checkUserName/'+ inputUserName,
+            type : 'post',
+            cache : false,
+            success : function(data) {
+                if (data.data == 0) {
+                    $(".checkNameComment").text("사용가능한 이름입니다.");
+                    $(".checkNameComment").css("color", "green");
+                    $("#doubleCheckName").val("true");
+                } else {
+                    $(".checkNameComment").text("사용중인 이름입니다 :p");
+                    $(".checkNameComment").css("color", "red");
+                    $("#doubleCheckName").val("false");
+                }
+            }, error : function() {
+                console.log("실패");
+            }
+        });
+    }
+});
+
+//비밀번호 확인
+$("#validPassword").blur(function(){
+    if($("#validPassword").val() == $("#password").val()){
+        $(".checkPwdComment").text("비밀번호가 일치합니다.");
+        $(".checkPwdComment").css("color", "green");
+        $("#doubleCheckPwd").val("true");
+    }else{
+        $(".checkPwdComment").text("비밀번호가 일치하지 않습니다.");
+        $(".checkPwdComment").css("color", "red");
+        $("#doubleCheckPwd").val("false");
+    }
+});
+
+var code = "";
+$("#checkEmailBtn").click(function(){
+    var inputEmail = $("#email").val();
+    $.ajax({
+        type:"GET",
+        url:"/auth/checkMail/" + inputEmail,
+        cache : false,
+        success:function(data){
+            if(data == "error"){
+                alert("이메일 주소가 올바르지 않습니다. 유효한 이메일 주소를 입력해주세요.");
+                $("#email").attr("autofocus",true);
+                $(".checkEmailComment").text("유효한 이메일 주소를 입력해주세요.");
+                $(".checkEmailComment").css("color","red");
+            }else{
+                alert("인증번호 발송이 완료되었습니다.\n입력한 이메일에서 인증번호 확인을 해주십시오.");
+                $("#validEmail").attr("disabled",false);
+                $("#validEmailBtn").css("display","inline-block");
+                $(".checkEmailComment").text("인증번호를 입력한 뒤 이메일 인증을 눌러주십시오.");
+                $(".checkEmailComment").css("color","green");
+                code = data.data;
+            }
+        }
+    });
+});
+
+//이메일 인증번호 대조
+$("#validEmailBtn").click(function(){
+    if($("#validEmail").val() == code){
+        $(".checkEmailComment").text("인증번호가 일치합니다.");
+        $(".checkEmailComment").css("color","green");
+        $("#doubleCheckEmail").val("true");
+        $("#validEmail").attr("disabled",true);
+    }else{
+        $(".checkEmailComment").text("인증번호가 일치하지 않습니다. 확인해주시기 바랍니다.");
+        $(".checkEmailComment").css("color","red");
+        $("#doubleCheckEmail").val("false");
+        $("#validEmail").attr("autofocus",true);
+    }
+});
+
 index.init();
--- src/main/webapp/WEB-INF/views/layout/header.jsp
+++ src/main/webapp/WEB-INF/views/layout/header.jsp
@@ -11,8 +11,15 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        $(function () {
+            $("#datepicker").datepicker({ dateFormat: 'yy-mm-dd' });
+        });
+    </script>
 </head>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-bs4.min.css" rel="stylesheet">
